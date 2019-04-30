0ff9612af68ebd23b27105b23dab5cd3bf802fd8
Frédérick LEMASSON <frederick.lemasson@dolmen-tech.com>
2019-04-30 14:35:17 +0200

    update for 2019 reality

--- jenkins_wrapper.sh
+++ jenkins_wrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Wrapper for sending the results of an arbitrary script/command to Jenkins for
 # monitoring. 
 #
@@ -24,7 +24,8 @@
 #
 # Forked for minor corrections (was almost perfect) from :
 #   http://github.com/joemiller/hudson_wrapper
-# 
+#
+#  Jenkins configuration : create the user JENKINS_WRAPPER_USER :  Configurer la sécurité globale / CSRF Protection off 
 
 if [ $# -lt 2 ]; then
     echo "Not enough args!"
@@ -34,13 +35,19 @@ fi
 
 
 #JENKINS_URL=$1; shift
-JENKINS_URL="http://myjenkinsfqdn:8080"
+JENKINS_URL="http://localhost:8080"
+#JENKINS_WRAPPER_USER="jen"
+JENKINS_WRAPPER_USER="admin"
+#SECRETPASSWORD="barber"
+SECRETPASSWORD="4b7959ebb25d4673a840abf9cb34b285"
 JOB_NAME=$1; shift
 SCRIPT="$@"
+CURL_AUTH_OPS="--user $JENKINS_WRAPPER_USER:$SECRETPASSWORD"
+CURL_AUTH_OPTS=${CURL_AUTH_OPTS:="--user $JENKINS_WRAPPER_USER:$SECRETPASSWORD"}
 
 
 # check if jenkins is running
-if curl -s --head $JENKINS_URL | grep "HTTP/1.1 200 OK" > /dev/null 2>&1
+if curl -s --head --user $JENKINS_WRAPPER_USER:$SECRETPASSWORD $JENKINS_URL | grep "HTTP/1.1 200 OK" > /dev/null 2>&1
     then
 		echo "Jenkins on $JENKINS_URL is OK :) ... lets go on ..."
     else
@@ -53,12 +60,12 @@ fi
 # requires it.  Otherwise, leave it blank (CURL_AUTH_OPTS="").  You can also override this
 # by setting it in your environment before calling this script
 
-CURL_AUTH_OPTS=${CURL_AUTH_OPTS:="--user JENKINS_WRAPPER_USER:SECRETPASSWORD"}
+#CURL_AUTH_OPTS=${CURL_AUTH_OPTS:="--user $JENKINS_WRAPPER_USER:$SECRETPASSWORD"}
 #CURL_AUTH_OPTS=""
 
 HOSTNAME=$(hostname)
 
-## encode any whitespace in the job name for URLs
+echo Encode any whitespace in the job name for URLs
 JOB_NAME=$(echo $HOSTNAME"_"$JOB_NAME | sed -e 's/[        ][      ]*/%20/g')
 
 OUTFILE=$(mktemp -t jenkins_wrapper.XXXXXX)
@@ -68,28 +75,25 @@ echo "Script being run: $SCRIPT"    >> $OUTFILE
 echo "Host            : $HOSTNAME"  >> $OUTFILE
 echo "" >> $OUTFILE
 
-### Execute the given script, capturing the result and how long it takes.
+echo Execute the given script, capturing the result and how long it takes.
 
-START_TIME=$(date +%s.%N)
-START_TIME_NICE=$(date --rfc-3339=ns)
+START_TIME=$(date +"%s.%N")
+START_TIME_NICE=$(date)
 eval $SCRIPT >> $OUTFILE 2>&1
 RESULT=$?
-END_TIME=$(date +%s.%N)
-END_TIME_NICE=$(date --rfc-3339=ns)
+END_TIME=$(date +"%s.%N")
+END_TIME_NICE=$(date)
 ELAPSED_MS=$(echo "($END_TIME - $START_TIME) * 1000 / 1" | bc)
 echo "" >> $OUTFILE
 echo "Start time: $START_TIME_NICE     ($START_TIME)" >> $OUTFILE
 echo "End time  : $END_TIME_NICE     ($END_TIME)" >> $OUTFILE
 echo "Elapsed ms: $ELAPSED_MS"  >> $OUTFILE
 
-### Post the results of the command to Jenkins.
-
-# We build up our XML payload in a temp file -- this helps avoid 'argument list
-# too long' issues.
+echo Post the results of the command to Jenkins.
 CURLTEMP=$(mktemp -t jenkins_wrapper_curl.XXXXXXXX)
 echo "<run><log encoding=\"hexBinary\">$(od -v -t xC $OUTFILE | sed '$d; s/^[0-9]* //' | tr -d ' \n\r')</log><result>${RESULT}</result><duration>${ELAPSED_MS}</duration></run>" > $CURLTEMP
 
-### create job if it does not exist
+echo Create job if it does not exist
 http_code=$(curl -s -o /dev/null -w'%{http_code}' -X POST ${CURL_AUTH_OPTS} ${JENKINS_URL}/job/${JOB_NAME})
 
 if [ "${http_code}" = "404" ]; then
